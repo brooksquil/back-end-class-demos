fd86f3255ba9ab66eb1c0210d0879586f9f2377e
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-10-17 15:16:09 -0500

    rest of todays notes

--- lootbag-api/app/controllers/children_controller.rb
+++ lootbag-api/app/controllers/children_controller.rb
@@ -5,6 +5,31 @@ class ChildrenController < ApplicationController
     end
 
     def create 
-        @child = Child.create(params)
+        @child = Child.create(params.permit(:name, :delivered))
+        json_response(@child)
+        
+    end
+
+    def show
+        # this allows specifying id in url
+        @child =
+        Child.find(params[:id])
+        json_response(@child)
+    end
+
+    def update
+        @child = 
+        Child.find(params[:id])
+        @child.update(params.permit(:name, :delivered))
+        json_response(@child)
+    end
+
+    def destroy
+        # destroy deletes entire record
+        @child = Child.find(params[:id])
+        @child.destroy
     end
 end
+
+
+# params.permit - allows access to specific attributes, otherwise the attributes are set.
