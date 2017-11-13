8900de5a55b4631b02156e3705b9a91e9ceca477
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-11-13 13:17:18 -0600

    add changes

--- new_blog/blog/config/routes.rb
+++ new_blog/blog/config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
+  resources :sessions
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+
   get 'welcome/index'
 
   root 'welcome#index'
 
   resources :articles do
-    resources :comments
+      resources :comments
   end
-  # then rails routes should be correct
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
